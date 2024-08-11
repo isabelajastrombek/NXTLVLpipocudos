c2d0717e2744ca4ccd19dfbad81652272dd325a3
Alyss <alyss@Alysss-MacBook-Air.local>
2024-08-11 09:43:17 -0300

    animacao generica de patinho andando

--- scenes_and_controllers/player_controller.gd
+++ scenes_and_controllers/player_controller.gd
@@ -3,15 +3,20 @@ extends CharacterBody2D
 var speed = 300
 var click_position = Vector2()
 var target_position = Vector2()
+@onready var animation_player = $AnimationPlayer
 
 func _ready():
 	click_position = position
-
+	
+	
 func _physics_process(delta):
 	if Input.is_action_just_pressed("click_left"):
-		click_position = get_global_mouse_position() 
+		click_position = get_global_mouse_position()
 		
 	if position.distance_to(click_position) > 3:
 		target_position = (click_position - position).normalized()
 		velocity = target_position * speed
-		move_and_slide() 
+		move_and_slide()
+		animation_player.play("player_walk")
+	else:
+		animation_player.stop(false)
