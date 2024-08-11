bf52bb876a4342ae82e582370e06fcf7787e1c84
Alyss <alyss@Alysss-MacBook-Air.local>
2024-08-11 14:59:42 -0300

    patinho flipado obaa ^-^

--- scenes_and_controllers/player_controller.gd
+++ scenes_and_controllers/player_controller.gd
@@ -1,22 +1,26 @@
 extends CharacterBody2D
 
-var speed = 300
-var click_position = Vector2()
+@export var speed = 300
+@export var distance_threshold = 10
 var target_position = Vector2()
+var direction = Vector2()
 @onready var animation_player = $AnimationPlayer
 
 func _ready():
-	click_position = position
-	
+	target_position = position
 	
 func _physics_process(delta):
 	if Input.is_action_just_pressed("click_left"):
-		click_position = get_global_mouse_position()
+		target_position = get_global_mouse_position()
 		
-	if position.distance_to(click_position) > 3:
-		target_position = (click_position - position).normalized()
-		velocity = target_position * speed
+	if position.distance_to(target_position) > distance_threshold:
+		direction = (target_position - position).normalized()
+		velocity = direction * speed
 		move_and_slide()
 		animation_player.play("player_walk")
+		if position.x > target_position.x:
+			$Sprite2D.flip_h = false
+		elif position.x < target_position.x:
+			$Sprite2D.flip_h = true
 	else:
 		animation_player.play("player_idle")
