03f07f536a8b78f307907d250ee8aab0d536ff13
Rafael Teivfik MacOS <rafael.teivfik2@gmail.com>
2024-08-11 21:21:21 -0300

    Adicionados assets, e outras coisas ajustadas

--- scenes_and_controllers/player_controller.gd
+++ scenes_and_controllers/player_controller.gd
@@ -3,8 +3,10 @@ class_name PlayerController
 
 @export var speed = 300
 @export var distance_threshold = 10
-var target_position = Vector2()
-var direction = Vector2()
+@export var walk_animation_threshold = 1
+var target_position := Vector2()
+var direction := Vector2()
+var last_position : Vector2
 @onready var animation_player = $AnimationPlayer
 
 signal moved(to : Vector2)
@@ -12,15 +14,19 @@ signal moved(to : Vector2)
 func force_set_position(new_pos: Vector2):
 	position = new_pos
 	target_position = new_pos
+	last_position = new_pos
 
 func _ready():
 	target_position = position
+	last_position = position
 	DialogueManager.set_player_controller(self)
 	
 func _physics_process(_delta):
 	if !DialogueManager.is_dialogue_active and Input.is_action_pressed("click_left"):
 		target_position = get_global_mouse_position()
 	_move_to_target()
+	_handle_animations()
+	last_position = position
 	
 func _move_to_target():
 	if position.distance_to(target_position) > distance_threshold:
@@ -28,10 +34,14 @@ func _move_to_target():
 		velocity = direction * speed
 		move_and_slide()
 		moved.emit(target_position)
-		animation_player.play("player_walk")
 		if position.x > target_position.x:
-			$Sprite2D.flip_h = false
+			%Sprite2D.flip_h = false
 		elif position.x < target_position.x:
-			$Sprite2D.flip_h = true
+			%Sprite2D.flip_h = true
+
+func _handle_animations():
+	var distance_walked = position-last_position
+	if distance_walked.length() >= walk_animation_threshold:
+		animation_player.play("player_walk")
 	else:
 		animation_player.play("player_idle")
